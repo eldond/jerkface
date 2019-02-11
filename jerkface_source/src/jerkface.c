f3f326db799469e94bb80b185df89f3052a39300
David Eldon <eldond@fusion.gat.com>
2019-02-11 09:35:25 -0800

    Replace hello.c with jerkface.c

--- jerkface_source/src/jerkface.c
+++ jerkface_source/src/jerkface.c
@@ -1,4 +1,4 @@
-/* hello.c -- print a greeting message and exit.
+/* jerkface.c -- print a greeting message and exit.
 
    Copyright 1992, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2005,
    2006, 2007, 2008, 2010, 2011, 2013, 2014 Free Software Foundation,
